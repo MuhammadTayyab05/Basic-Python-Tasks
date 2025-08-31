d7aacbbc08f76da786ee9aea335b942fd3ae7847
Muhammad Tayyab <m.tayyab0505@gmail.com>
2025-08-31 11:57:01 +0500

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,49 @@
+import pdb
+num1=int(input("Enter first number:"))
+num2=int(input("enter second number:"))
+sum=num1+num2
+print("Total sum is:",sum)
+
+a=2.2
+print(int(a))
+
+num1=int(input("Enter first number:"))
+if(num1%2==0):
+    print("even")
+else:
+    print("ODD")
+
+a=float(input("first num is :"))
+b=float(input("second num is:"))
+c=float(input("third num is:"))
+d=float(input("enter fourth::"))
+if(a>b and a>c and a>d):
+    print("first A is greator")
+elif(b>c and b>d):
+    print("B is greater")
+elif(c>d):
+    print('C is greater')
+else:
+    print("D is greater")
+
+num1=str(input("Enter first number:"))
+vowels=('a','e','i','o','u')
+# if(num1==vowels):
+
+txt = input(str(print("enter the string "))) 
+if txt ==(txt[::-1]):
+    print("plaindrome ")
+
+else:
+    print("no its not :")
+for i in txt:
+
+    if i in ('AEIOUaeiou'):
+        V +=1
+
+print(V)
+
+
+
+
+
